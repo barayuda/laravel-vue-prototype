a7547697786923e4d7eaef038f9de9a6443a5c04
Barayuda <saya.bara@gmail.com>
2018-04-23 14:27:09 +0800

    Add mockup data

--- prototype/sample/data.js
+++ prototype/sample/data.js
@@ -1,10 +1,18 @@
 var sample = {
   title: 'Mû Boutique Resort - Bali',
   address: 'Jl. Pantai Bingin, Pecatu 80364 Bali Indonesia',
-  about: 'MU according to a legend is a once lost continent in the Pacific Ocean, a dream paradise that\n' +
-  'disappeared under water. Immerse yourself in the tranquility at Mu Boutique Resort’s\n' + 
-  'exclusive & secluded cliff top location, soak in the spectacular views, experience the ocean’s\n' +
-  'soothing qualities and begin your dream holiday here.\r\n\r\n',
+  about: 'MU according to a legend is a once lost continent in the Pacific Ocean, a dream paradise that ' +
+  'disappeared under water. Immerse yourself in the tranquility at Mu Boutique Resort’s ' + 
+  'exclusive & secluded cliff top location, soak in the spectacular views, experience the ocean’s ' +
+  'soothing qualities and begin your dream holiday here.\r\n\r\n' +
+  'Come and stay at this modern and comfortable apartment! My home is centrally located right in the middle ' +
+  'of the downtown. Talk about convenience! Shops, stores, and other destination areas are nearby. \r\n\r\nFeel the ' +
+  'warmth of the sun as there is plenty of natural light showers. The living room features tv, sofa, table, radio, ' +
+  'and fan. There is free wi-fi with a fast internet speed. \r\n\r\nForgot shopping for breakfast staples? We provide ' +
+  'eggs, bread, cereals, coffee, milk, tea and cookies. Enjoy cooking as my kitchen boasts full size appliances. The ' +
+  'dining table is for four people. Need to wash your clothes? There is a washer and a dryer. We provide hampers, ' +
+  'detergents, and clothing conditioner. \r\n\r\nIf you need to hit the gym, there is located at the fourth floor of ' +
+  'the building. There is indoor spa and pool',
   amenities: [
     {
       title: 'Wireless Internet',
