af689c29752037c5fb0bfe499c51dd3e80d05838
Barayuda <saya.bara@gmail.com>
2018-04-23 14:08:49 +0800

    Add simple prototype

---
+++ prototype/sample/data.js
@@ -0,0 +1,52 @@
+var sample = {
+  title: 'Mû Boutique Resort - Bali',
+  address: 'Jl. Pantai Bingin, Pecatu 80364 Bali Indonesia',
+  about: 'MU according to a legend is a once lost continent in the Pacific Ocean, a dream paradise that\n' +
+  'disappeared under water. Immerse yourself in the tranquility at Mu Boutique Resort’s\n' + 
+  'exclusive & secluded cliff top location, soak in the spectacular views, experience the ocean’s\n' +
+  'soothing qualities and begin your dream holiday here.\r\n\r\n',
+  amenities: [
+    {
+      title: 'Wireless Internet',
+      icon: 'fa-wifi'
+    },
+    {
+      title: 'Pets Allowed',
+      icon: 'fa-paw'
+    },
+    {
+      title: 'TV',
+      icon: 'fa-television'
+    },
+    {
+      title: 'Kitchen',
+      icon: 'fa-cutlery'
+    },
+    {
+      title: 'Breakfast',
+      icon: 'fa-coffee'
+    },
+    {
+      title: 'Laptop friendly workspace',
+      icon: 'fa-laptop'
+    }
+  ],
+  prices: [
+    {
+      title: 'Per night',
+      value: '$89'
+    },
+    {
+      title: 'Extra people',
+      value: 'No charge'
+    },
+    {
+      title: 'Weekly discount',
+      value: '18%'
+    },
+    {
+      title: 'Monthly discount',
+      value: '50%'
+    }
+  ]
+};
